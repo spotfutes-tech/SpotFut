cb14de90fcd0ffc34abddbd541f096d99b50d594
spotfutes-tech <farhan.spotfut.es@gmail.com>
2026-02-28 13:14:58 +0100

    Create product.js

---
+++ product.js
@@ -0,0 +1,22 @@
+const PRODUCTS = {
+
+  /* Jackets */
+  "varsity-black": { name: "Varsity Black Edition", price: 59 },
+  "varsity-green": { name: "Varsity Green Edition", price: 59 },
+  "varsity-blue": { name: "Varsity Royal Blue Edition", price: 59 },
+  "varsity-latte": { name: "Varsity Latte Edition", price: 59 },
+  "varsity-solar": { name: "Varsity Solar Edition", price: 59 },
+  "varsity-sky": { name: "Varsity Sky Edition", price: 59 },
+
+  /* Standard Kits */
+  "KIT-001": { name: "Black Phantom", price: 39 },
+
+  /* Signature Elite Kits */
+  "SF-SJM-S001": { name: "Elite Pro Fusion Edition", price: 11 },
+  "SF-SJM-S004": { name: "Hexa Pulse Edition", price: 11 },
+  "SF-SJM-S003": { name: "Urban Strike Gradient Edition", price: 11 },
+  "SF-SJM-S005": { name: "Aurum Pro Edition", price: 11 },
+  "SF-SJM-S007": { name: "Aqua Storm Edition", price: 11 },
+  "SF-SJM-S008": { name: "Crimson Bolt Edition", price: 11 }
+
+};
