1fbe437eeb48fa1bdf6bb2baf5717ed5dcf5c398
spotfutes-tech <farhan.spotfut.es@gmail.com>
2026-03-01 15:53:39 +0100

    Update products.js

--- products.js
+++ products.js
@@ -17,4 +17,20 @@ const PRODUCTS = {
   "SF-SJM-S007": { name: "Aqua Storm Edition", price: 11 },
   "SF-SJM-S008": { name: "Crimson Bolt Edition", price: 11 }
 
+  "SF-SJF-S001": { name: "Blush Strike Edition", price: 11 },
+  "SF-SJF-S002": { name: "Phantom Fade Edition", price: 11 },
+  "SF-SJF-S004": { name: "Storm Fit Edition", price: 11 },
+
+  "SF-SJK-S001": { name: "Junior Strike Pro Edition", price: 11 },
+  "SF-SJK-S003": { name: "Junior Fire Start Edition", price: 11 },
+  "SF-SJK-S004": { name: "Junior Velocity Flame Edition", price: 11 },
+
+ "SF-SJK-S101": { name: "Junior Spark Edge Edition", price: 11 },
+  "SF-SJK-S102": { name: "Junior Nexus Rush Edition", price: 11 },
+  "SF-SJK-S103": { name: "Junior Tide Bolt Edition", price: 11 },
+
+
+
+  
+
 };
