0b4ffaad67b52cb11dc95befc8b44c1b9af399bb
spotfutes-tech <farhan.spotfut.es@gmail.com>
2026-04-25 18:12:17 +0200

    Update products.js

--- products.js
+++ products.js
@@ -178,7 +178,7 @@ const CATEGORIES = {
   /* FOOTBALL */
   football_kits_men: ["SF-SJM-S001a","SF-SJM-S002a","SF-SJM-S003a","SF-SJM-S004a","SF-SJM-S005a",
                      "SF-SJM-S006a","SF-SJM-S007a","SF-SJM-S008a","SF-SJM-S009a","SF-SJM-S010a","SF-SJM-S011a"],
-  football_tracksuits_men: ["SF-CTS-M039a"],
+  football_tracksuits_men: ["SF-CTS-M039a","SF-CTS-M039a","SF-CTS-M039a"],
   football_hoodies_men: ["SF-SJM-S005a"],
 
   football_jackets_men: [
