45ebebea4a3b41102b830d1729582898a7d7fad5
spotfutes-tech <farhan.spotfut.es@gmail.com>
2026-04-28 15:32:12 +0200

    Update products.js

--- products.js
+++ products.js
@@ -239,7 +239,7 @@ const PRODUCTS = {
 "SF-SJK-S106a":{"name":"Soccer Junior Solar Drift Edition","price":11,"img":["Soccer_Hero/Junior_Images/SF-SJK-S106_1.jpg","Soccer_Hero/Junior_Images/SF-SJK-S106_2.jpg","Soccer_Hero/Junior_Images/SF-SJK-S106_3.jpg","Soccer_Hero/Junior_Images/SF-SJK-S106_4.jpg"]},
 
       /* 11111111111111....... Bags - Accessoris .........................*/
-"SF-BP-0001a":{"name":"SF Classic Backpack","price":19,category: "bags","img":["Accessories/Images_Bags/SF-BP-0001-1.png","Accessories/Images_Bags/SF-BP-0001-2.png","Accessories/Images_Bags/SF-BP-0001-3.png"]},
+"SF-BP-0001a":{"name":"SF Classic Backpack","price":19,category: "bags","img":["Accessories/Images_Bags/SF-BP-0001-4.png","Accessories/Images_Bags/SF-BP-0001-5.png","Accessories/Images_Bags/SF-BP-0001-6.png"]},
 "SF-BP-1001a":{"name":"SF Urban Edge Backpack","price":19,category: "bags","img":["Accessories/Images_Bags/SF-BP-1001-1.png","Accessories/Images_Bags/SF-BP-1001-2.png","Accessories/Images_Bags/SF-BP-1001-3.png"]},
 "SF-BP-2001a":{"name":"SF Camo Backpack","price":29,category: "bags","img":["Accessories/Images_Bags/SF-BP-2001-1.png","Accessories/Images_Bags/SF-BP-2001-2.png"]},
 "SF-BP-3001a":{"name":"SF Camo Backpack","price":29,category: "bags","img":["Accessories/Images_Bags/SF-BP-3001-1.png","Accessories/Images_Bags/SF-BP-3001-2.png"]},
