6edd7a4230b64d2fa4da3bc6d114eacda2bfae54
spotfutes-tech <farhan.spotfut.es@gmail.com>
2026-05-01 16:15:10 +0200

    Update products.js

--- products.js
+++ products.js
@@ -246,7 +246,7 @@ const PRODUCTS = {
   "SF-SJF-S008a": {"name": "Soccer Static Strike Edition", "price": 11,  category: "soccer_kits_women", "img": ["Soccer_Hero/Women_Images/SF-SJF-S008_1.jpg", "Soccer_Hero/Women_Images/SF-SJF-S008_2.jpg", "Soccer_Hero/Women_Images/SF-SJF-S008_3.jpg", "Soccer_Hero/Women_Images/SF-SJF-S008_4.jpg"]},
   "SF-SJF-S009a": {"name": "Soccer Glacier Strike Edition", "price": 11,  category: "soccer_kits_women", "img": ["Soccer_Hero/Women_Images/SF-SJF-S009_1.jpg", "Soccer_Hero/Women_Images/SF-SJF-S009_2.jpg", "Soccer_Hero/Women_Images/SF-SJF-S009_3.jpg", "Soccer_Hero/Women_Images/SF-SJF-S009_4.jpg"]},
   "SF-SJF-S010a": {"name": "Soccer Prism Pulse Edition", "price": 11,  category: "soccer_kits_women", "img": ["Soccer_Hero/Women_Images/SF-SJF-S010_1.png", "Soccer_Hero/Women_Images/SF-SJF-S010_2.jpg", "Soccer_Hero/Women_Images/SF-SJF-S010_3.jpg", "Soccer_Hero/Women_Images/SF-SJF-S010_4.jpg"]},
-"SF-SJF-S011a": {"name": "Soccer Prism Pulse Edition", "price": 11,  category: "soccer_kits_women", "img": ["Soccer_Hero/Women_Images/SF-SJF-S011_1.png", "Soccer_Hero/Women_Images/SF-SJF-S011_2.png", "Soccer_Hero/Women_Images/SF-SJF-S011_3.png"]},
+"SF-SJF-S011a": {"name": "Soccer Titan Noir Edition", "price": 11,  category: "soccer_kits_women", "img": ["Soccer_Hero/Women_Images/SF-SJF-S011_1.png", "Soccer_Hero/Women_Images/SF-SJF-S011_2.png", "Soccer_Hero/Women_Images/SF-SJF-S011_3.png"]},
 
 
     /* 11111111111111....... Women Mehs Vests .........................*/
